96ab44ee3cafca6cb19c89b32870cbb55923a5df
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2023-03-03 14:02:48 +0900

    [API Gateway]Add My API tests.
    - Request Validation

---
+++ apigateway/__tests__/api.spec.js
@@ -0,0 +1,43 @@
+const frisby = require('frisby');
+const Joi = require('joi');
+
+const apiId = 'API_ID';
+const region = 'REGION';
+const host = `https://${apiId}.execute-api.${region}.amazonaws.com`;
+const stage = 'STAGE'
+
+beforeAll(() => {
+  frisby.baseUrl(host);
+});
+
+describe('Amazon API Gateway Example', () => {
+  it('GET /test', () => {
+    const url = new URL(stage + '/test', host);
+    url.searchParams.append('q0', 'a');
+    return frisby.get(url)
+      //.inspectRequestHeaders()
+      //.inspectRequest()
+      //.inspectStatus()
+      //.inspectHeaders()
+      //.inspectBody()
+      .expect('status', 200)
+      .expect('header', 'Content-Type', 'application/json');
+  });
+  it('POST /test', () => {
+    const url = new URL(stage + '/test', host);
+    url.searchParams.append('q0', 'a');
+    return frisby.post(url, {
+      body: {
+        name: 'User',
+        type: 'AAA',
+      }
+    })
+      //.inspectRequestHeaders()
+      //.inspectRequest()
+      //.inspectStatus()
+      //.inspectHeaders()
+      //.inspectBody()
+      .expect('status', 200)
+      .expect('header', 'Content-Type', 'application/json');
+  });
+});
