dc97d2ca4c7d91f65fa4a3b03cefcd18d04fa954
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2023-03-29 23:07:13 +0900

    [Lambda]Powertools for AWS Lambda
    - Logger

--- lambda/function/event/__tests__/index.spec.js
+++ lambda/function/event/__tests__/index.spec.js
@@ -1,12 +1,41 @@
 const { handler } = require('../src/index');
 
+function lambdaContext() {
+  // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-context.html
+  return {
+    functionName: 'event',
+    functionVersion: '$LATEST',
+    invokedFunctionArn: 'arn:aws:lambda:REGION:ACCOUNT-ID:function:event',
+    memoryLimitInMB: 512,
+    awsRequestId: 'test-test',
+    logGroupName: '/aws/lambda/event',
+    logStreamName: 'test',
+    identity: null,
+    clientContext: null,
+  };
+}
+
 it('GET', async () => {
   const event = {};
-  // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-context.html
-  const context = {};
+  const context = lambdaContext();
   console.log('[Event]', event);
   console.log('[Context]', event);
   const res = await handler(event, context);
   console.log('[Response]', res);
+  expect(res.statusCode).toBe(200);
+});
+
+it.each([
+  ['critical'],
+  ['error'],
+  ['warn'],
+  ['info'],
+  ['debug'],
+  [''],
+])('Logger level: %s', async (level) => {
+  const event = {
+    queryStringParameters: { log: level },
+  };
+  const res = await handler(event, lambdaContext());
   expect(res.statusCode).toBe(200);
 });
--- lambda/function/event/src/index.js
+++ lambda/function/event/src/index.js
@@ -1,6 +1,31 @@
+const { Logger } = require('@aws-lambda-powertools/logger');
 const _ = require('lodash');
 
+const logger = new Logger();
+
 exports.handler = async (event/* Request */, context) => {
+  logger.addContext(context);
+
+  // Logger
+  /* eslint-disable no-fallthrough */
+  switch (event.queryStringParameters?.log) {
+  case 'critical':
+    logger.critical('[CRITICAL]', event);
+  case 'error':
+    logger.error('[ERROR]', event);
+  case 'warn':
+    logger.warn('[WARN]', event);
+  case 'info':
+    logger.info('[INFO]', event);
+  case 'debug':
+    logger.debug('[DEBUG]', event);
+    break;
+  default:
+    console.log('[console.log]', event);
+    break;
+  }
+  /* eslint-enable no-fallthrough */
+
   // Response
   const response = {
     statusCode: 200,
