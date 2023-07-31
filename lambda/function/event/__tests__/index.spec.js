68bc2a164621898b5e8c12fe229553a50ff66eaf
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2023-07-31 19:44:34 +0900

    [Lambda]Powertools for AWS Lambda
    - Parameters

--- lambda/function/event/__tests__/index.spec.js
+++ lambda/function/event/__tests__/index.spec.js
@@ -47,3 +47,11 @@ it.each([
   const res = await handler(event, lambdaContext());
   expect(res.statusCode).toBe(200);
 });
+
+it('Parameters', async () => {
+  const event = {
+    queryStringParameters: { ssm: true },
+  };
+  const res = await handler(event, lambdaContext());
+  expect(res.statusCode).toBe(200);
+});
