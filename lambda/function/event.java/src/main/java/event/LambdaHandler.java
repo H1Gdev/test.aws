106ace786b1e546fd0812f7f9c338c1866544c66
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2023-09-07 14:40:04 +0900

    [Lambda]Add code check using Checkstyle(Java).
    - fix.

--- lambda/function/event.java/src/main/java/event/LambdaHandler.java
+++ lambda/function/event.java/src/main/java/event/LambdaHandler.java
@@ -12,6 +12,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import java.net.HttpURLConnection;
 import java.util.Map;
 import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
@@ -24,7 +25,7 @@ import software.amazon.awssdk.services.sts.model.GetCallerIdentityResponse;
 import software.amazon.lambda.powertools.logging.Logging;
 import software.amazon.lambda.powertools.parameters.Param;
 
-public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public final class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     private static final Logger LOG = LogManager.getLogger();
 
     // Parameters
@@ -48,22 +49,22 @@ public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent
 
         // Logging
         switch (Optional.ofNullable(event.getQueryStringParameters()).map(p -> p.get("log")).orElse(StringUtils.EMPTY)) {
-            case "fatal":
-                LOG.fatal("[FATAL]" + event);
-            case "error":
-                LOG.error("[ERROR]" + event);
-            case "warn":
-                LOG.warn("[WARN]" + event);
-            case "info":
-                LOG.info("[INFO]" + event);
-            case "debug":
-                LOG.debug("[DEBUG]" + event);
-            case "trace":
-                LOG.trace("[TRACE]" + event);
-                break;
-            default:
-                System.out.println("[System.out.println]" + event);
-                break;
+        case "fatal":
+            LOG.fatal("[FATAL]" + event);
+        case "error":
+            LOG.error("[ERROR]" + event);
+        case "warn":
+            LOG.warn("[WARN]" + event);
+        case "info":
+            LOG.info("[INFO]" + event);
+        case "debug":
+            LOG.debug("[DEBUG]" + event);
+        case "trace":
+            LOG.trace("[TRACE]" + event);
+            break;
+        default:
+            System.out.println("[System.out.println]" + event);
+            break;
         }
 
         // Parameters
@@ -75,16 +76,15 @@ public class LambdaHandler implements RequestHandler<APIGatewayProxyRequestEvent
         // Response
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
         response.setIsBase64Encoded(false);
-        response.setStatusCode(200);
+        response.setStatusCode(HttpURLConnection.HTTP_OK);
         Map<String, String> headers = Map.of("Content-Type", "application/json");
         response.setHeaders(headers);
-        response.setBody(
-	        "{" +
-	        "  \"event\": {" +
-	        "    \"body\": \"" + StringUtils.defaultString(event.getBody()).replace("\"", "") + "\"" +
-	        "  }," +
-	        "  \"context\": \"" + context + "\"" +
-	        "}"
+        response.setBody("{"
+                +        "  \"event\": {"
+                +        "    \"body\": \"" + StringUtils.defaultString(event.getBody()).replace("\"", "") + "\""
+                +        "  },"
+                +        "  \"context\": \"" + context + "\""
+                +        "}"
         );
         return response;
     }
